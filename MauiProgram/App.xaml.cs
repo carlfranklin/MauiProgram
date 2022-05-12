f3b80c6febd015064f0fe59a86989836565ca5ba
Carl Franklin <carl@franklins.net>
2022-05-12 17:13:43 +0100

    Add project files.

---
+++ MauiProgram/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiProgram;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiProgram/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiProgram;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Consolas-Regular.ttf", "ConsolasRegular");
+				fonts.AddFont("Consola-Bold.ttf", "ConsolasBold");
+				fonts.AddFont("Consolas-Italic.ttf", "ConsolasItalic");
+				fonts.AddFont("Consolas-Bold-Italic.ttf", "ConsolasBoldItalic");
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
