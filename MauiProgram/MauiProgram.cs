b01f6d0a6c57e5a4391470322391f3d07b9672e5
Carl Franklin <carl@franklins.net>
2022-05-12 17:31:00 +0100

    bold again

--- MauiProgram/MauiProgram.cs
+++ MauiProgram/MauiProgram.cs
@@ -10,7 +10,7 @@ public static class MauiProgram
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("Consolas-Regular.ttf", "ConsolasRegular");
-				fonts.AddFont("Consola-Bold.ttf", "ConsolasBold");
+				fonts.AddFont("Consolas-Bold.ttf", "ConsolasBold");
 				fonts.AddFont("Consolas-Italic.ttf", "ConsolasItalic");
 				fonts.AddFont("Consolas-Bold-Italic.ttf", "ConsolasBoldItalic");
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
